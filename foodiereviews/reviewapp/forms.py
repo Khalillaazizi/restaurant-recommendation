bff930ab2cdc2753abff9737e116fe83f34d2d6c
KHALIL <khalil.iron@hotmail.com>
2023-05-03 19:30:11 +0200

    first commit

---
+++ foodiereviews/reviewapp/forms.py
@@ -0,0 +1,23 @@
+from django.forms import ModelForm, Textarea
+from .models import Review,Restaurant
+from django import forms
+from .models import User
+
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = [
+            'review_title',
+            'review_description',
+        ]
+        widgets = {
+            'review_description': Textarea(attrs={'cols': 80, 'rows': 8}),
+        }
+
+class RestaurantForm(forms.ModelForm):
+    restaurant_text= forms.CharField(widget= forms.TextInput(attrs={'class':'form-control'}))
+    restaurant_address= forms.CharField(widget= forms.TextInput(attrs={'class':'form-control'}))   
+    class Meta:
+        model = Restaurant
+        fields = '__all__'
---
+++ foodiereviews/reviewapp/urls.py
@@ -0,0 +1,35 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'reviewapp'
+urlpatterns = [
+    
+    path('', views.inde, name= 'inde'),
+    path('index/', views.index, name= 'index'),
+    
+    path('addnew/',views.addnew, name='addnew'),
+    
+    path('home/', views.home, name='home'),
+    
+    
+    path('resto/<int:restaurant_id>/', views.details, name='details'),
+
+    
+    path('resto/<int:restaurant_id>/add/', views.add, name='add'),
+
+   
+    path('resto/<int:restaurant_id>/reviewed/', views.reviewed, name='reviewed'),
+
+   
+    path('api/comment/add/', views.CommentAdd.as_view(), name="comment"),
+    
+
+    path('api/reply/add/', views.ReplyAdd.as_view(), name="reply"),
+
+
+    path('api/like/add/', views.LikeAdd.as_view(), name="like"),
+
+    path('api/restaurants/list/', views.GetRestaurantsByCategory.as_view(), name="restaurants_by_category"),
+    
+]
